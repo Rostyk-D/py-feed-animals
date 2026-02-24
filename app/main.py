c45a0c45803781627b5674e676723ed1d2d1e9b4
Rostyk <rostik.durjagin@gmail.com>
2026-02-24 19:09:30 +0200

    Fix

--- app/main.py
+++ app/main.py
@@ -36,5 +36,5 @@ class Dog(Animal):
         print("The slippers delivered!")
 
 
-def feed_animals(animals: list[Animal]) -> int:
+def feed_animals(animals: List[Animal]) -> int:
     return sum(animal.feed() for animal in animals)
